e8de1c8ac6bfab9af6679113874d0e17bdc691eb
root <root@NC-Bostjan>
2024-01-21 22:19:09 +0100

    add family members

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\FamilyMembers;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+Route::get('/family-members', FamilyMembers::class)->name('family-members');
